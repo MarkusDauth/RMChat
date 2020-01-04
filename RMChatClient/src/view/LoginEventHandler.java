5dc88115a18a6744f92e063b130e3acb951cb7c7
RaschiedSlet <57643674+RaschiedSlet@users.noreply.github.com>
2020-01-04 18:09:32 +0100

    login data now gets checked before it is sent to the server

--- RMChatClient/src/view/LoginEventHandler.java
+++ RMChatClient/src/view/LoginEventHandler.java
@@ -4,6 +4,7 @@ import controller.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import model.LoginData;
+import properties.Properties;
 
 import java.io.IOException;
 
@@ -34,9 +35,26 @@ public class LoginEventHandler {
 
     @FXML
     private void handleLoginButton() {
-        LoginData loginData = new LoginData();
-        loginData.setUsername(usernameField.getText());
-        loginData.setPassword(passwordField.getText());
-        controller.loginUser(loginData);
+        if(isUsernameAndPasswordCorrect()) {
+            LoginData loginData = new LoginData();
+            loginData.setUsername(usernameField.getText());
+            loginData.setPassword(passwordField.getText());
+            controller.loginUser(loginData);
+        }
+        else{
+            views.showMessage("InvalidUsernameOrPassword");
+        }
+    }
+    private boolean isUsernameAndPasswordCorrect(){
+        return checkUsername() && checkPassword();
+    }
+
+    private boolean checkPassword(){
+        return usernameField.getText().length() >= Properties.getInt("username.minLength") &&
+                usernameField.getText().length() < Properties.getInt("username.maxLength");
+    }
+    private boolean checkUsername(){
+        return passwordField.getText().length() >= Properties.getInt("password.minLength") &&
+                passwordField.getText().length() < Properties.getInt("password.maxLength");
     }
 }
